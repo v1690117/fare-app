7d8faeea7425c8a5c6a4a7c0bf75d51e4e53b541
v1690117 <v1690117@outlook.com>
2025-01-30 22:22:07 +0300

    try using expo camera

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,166 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Button, Image, PixelRatio, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
+import {HelloWave} from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+
+import {CameraType, CameraView, useCameraPermissions} from 'expo-camera';
+import {useRef, useState} from "react";
+
+import {captureRef} from 'react-native-view-shot';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+    const imageContainer = useRef<View>(null);
+    const [capturedImage, setCapturedImage] = useState<string | null>(null);
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <ParallaxScrollView
+            headerBackgroundColor={{light: '#A1CEDC', dark: '#1D3D47'}}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/partial-react-logo.png')}
+                    style={styles.reactLogo}
+                />
+            }>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Welcome!</ThemedText>
+                <HelloWave/>
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+                <ThemedText>
+                    Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+                    Press{' '}
+                    <ThemedText type="defaultSemiBold">
+                        {Platform.select({
+                            ios: 'cmd + d',
+                            android: 'cmd + m',
+                            web: 'F12'
+                        })}
+                    </ThemedText>{' '}
+                    to open developer tools.
+                </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+                <ThemedText>
+                    Tap the Explore tab to learn more about what's included in this starter app.
+                </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+                <ThemedText>
+                    When you're ready, run{' '}
+                    <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+                    <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+                    <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+                    <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+                </ThemedText>
+            </ThemedView>
+        </ParallaxScrollView>
+    }
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission"/>
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+
+    const takePicture = async () => {
+        if (imageContainer.current) {
+            const targetPixelCount = 1080; // If you want full HD pictures
+            const pixelRatio = PixelRatio.get();
+            const pixels = targetPixelCount / pixelRatio;
+
+            const result = await captureRef(imageContainer, {
+                result: 'tmpfile',
+                height: pixels,
+                width: pixels,
+                quality: 1,
+                format: 'png',
+            });
+            setCapturedImage(result);
+        }
+    };
+
+    return (
+        <View style={styles.container} ref={imageContainer}>
+            <CameraView style={styles.camera} facing={facing}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={takePicture}>
+                        <Text style={styles.text}>Get Picture</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+            {capturedImage && <Image source={{uri: capturedImage}} style={styles.preview}/>}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    preview: {
+        marginTop: 20,
+        width: 200,
+        height: 300,
+    },
 });
